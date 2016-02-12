18bcdacdc41720c8ee087017b24e3570fda78f53
Dan Davison <dan@counsyl.com>
2016-02-12 01:23:32 -0800

    Initial version

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='iterm2-color-scheme',
+    version='0.0.1',
+    author='Dan Davison',
+    author_email='dandavison7@gmail.com',
+    description="Fast color scheme selector for iTerm2",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'iterm2-color-scheme = iterm2_color_scheme.iterm2_color_scheme:main',
+        ],
+    },
+)
