5868150618898c784b944dc3f7f59b9a275699fb
Dan Davison <dandavison7@gmail.com>
2016-02-13 20:58:08 -0800

    Add tab completion

--- iterm2_color_scheme/iterm2_color_scheme.py
+++ iterm2_color_scheme/iterm2_color_scheme.py
@@ -3,7 +3,9 @@ from collections import deque
 from collections import namedtuple
 from itertools import chain
 from itertools import starmap
+import logging
 import os
+import readline
 import subprocess
 import sys
 import termios
@@ -11,6 +13,41 @@ import tty
 
 
 UP_ARROW = '\x1b[A'
+
+logging.basicConfig(
+    filename='/tmp/iterm2-color-scheme.log',
+    level=logging.DEBUG,
+)
+
+
+class SimpleCompleter(object):
+
+    def __init__(self, options):
+        self.options = sorted(options)
+        return
+
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            # This is the first time for this text, so build a match list.
+            if text:
+                self.matches = [s
+                                for s in self.options
+                                if s and s.startswith(text)]
+                logging.debug('%s matches: %s', repr(text), self.matches)
+            else:
+                self.matches = self.options[:]
+                logging.debug('(empty input) matches: %s', self.matches)
+
+        # Return the state'th item from the match list,
+        # if we have that many.
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+        logging.debug('complete(%s, %s) => %s',
+                      repr(text), state, repr(response))
+        return response
 
 
 class Scheme(namedtuple('Scheme', ['index', 'path'])):
@@ -39,6 +76,10 @@ class ColorSchemeBrowser(object):
                                      enumerate(schemes_dir + '/' + f
                                                for f in os.listdir(schemes_dir)
                                                if f.endswith('.itermcolors'))))
+
+        readline.set_completer(SimpleCompleter(s.name for s in self.schemes)
+                               .complete)
+        readline.parse_and_bind('tab: complete')
 
         # A blank string that is long enough to conceal all other output
         self.blank = ' ' * max(chain((len(repr(s)) for s in self.schemes),
