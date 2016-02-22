e2dd36576f79b1c97bd20f1bf13ca9b60eb7fd80
Dan Davison <dan@counsyl.com>
2016-02-21 19:10:16 -0800

    Combine into single class

--- iterm2_color_scheme/iterm2_color_scheme.py
+++ iterm2_color_scheme/iterm2_color_scheme.py
@@ -20,41 +20,6 @@ class deque(collections.deque):
         self.rotate(list(self).index(item))
 
 
-class SimpleCompleter(object):
-
-    def __init__(self, options, hook_functions):
-        self.options = options
-        self.hook_functions = hook_functions
-
-    def complete(self, text, state):
-        response = None
-        if state == 0:
-            # This is the first time for this text, so build a match list.
-            if text:
-                self.matches = [s
-                                for s in self.options
-                                if s and text.lower() in s.lower()]
-                logging.debug('%s matches: %s', repr(text), self.matches)
-            else:
-                self.matches = self.options[:]
-                logging.debug('(empty input) matches: %s', self.matches)
-
-        # Return the state'th item from the match list,
-        # if we have that many.
-        try:
-            response = self.matches[state]
-        except IndexError:
-            response = None
-        else:
-            if len(self.matches) == 1:
-                for fn in self.hook_functions:
-                    fn(response)
-
-        logging.debug('complete(%s, %s) => %s',
-                      repr(text), state, repr(response))
-        return response
-
-
 class Scheme(namedtuple('Scheme', ['index', 'path'])):
     """
     An iTerm2 color scheme file.
@@ -80,9 +45,8 @@ class ColorSchemeSelector(object):
         self.scheme = next(iter(self.schemes))
 
         self.name_to_scheme = {s.name: s for s in self.schemes}
-        self.completer = SimpleCompleter(None, [self.apply_scheme])
-        self.completer.options = [s.name for s in self.schemes]
-        readline.set_completer(self.completer.complete)
+        self.completion_options = [s.name for s in self.schemes]
+        readline.set_completer(self.complete)
         readline.set_completer_delims('')
         readline.parse_and_bind('tab: complete')
         readline.parse_and_bind('j: menu-complete')
@@ -106,6 +70,36 @@ class ColorSchemeSelector(object):
                 sys.exit(0)
             except KeyError:
                 sys.exit(1)
+            else:
+                self.apply_scheme()
+
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            # This is the first time for this text, so build a match list.
+            if text:
+                self.matches = [s
+                                for s in self.completion_options
+                                if s and text.lower() in s.lower()]
+                logging.debug('%s matches: %s', repr(text), self.matches)
+            else:
+                self.matches = self.completion_options[:]
+                logging.debug('(empty input) matches: %s', self.matches)
+
+        # Return the state'th item from the match list,
+        # if we have that many.
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+        else:
+            if len(self.matches) == 1:
+                self.apply_scheme(response)
+
+        logging.debug('complete(%s, %s) => %s',
+                      repr(text), state, repr(response))
+        return response
+
 
     def apply_scheme(self, name=None):
         if name is not None:
