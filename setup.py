b3674f6fb27a9b2b601103eca79235b7b4b14968
Dan Davison <dandavison7@gmail.com>
2016-02-22 18:33:48 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='iterm2-color-scheme',
-    version='0.0.1',
+    version='0.0.2',
     author='Dan Davison',
     author_email='dandavison7@gmail.com',
     description="Fast color scheme selector for iTerm2",
